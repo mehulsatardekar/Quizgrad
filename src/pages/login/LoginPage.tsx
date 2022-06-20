13c7485cc47072182ce6495f410f99fe3fa74eed
mehulsatardekar <satardekar042197@gmail.com>
2022-06-20 21:19:30 +0530

    feat: login component with loginTypes for typeguard  and login page added

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Login } from '../../components/index';
+
+import './loginpage.css'
+const LoginPage = () => {
+  return (
+    <Login/>
+  )
+}
+
+export {LoginPage}
