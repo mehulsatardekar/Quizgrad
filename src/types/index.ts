7216c546813fac8d0feffdbf7b6eaeafb2c4ba5a
mehulsatardekar <satardekar042197@gmail.com>
2022-06-21 01:53:45 +0530

    feat: type guards for result-type and index file added

---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type {categoriesType, QuizCategoriesType, quizCategoriesDetailsType, CategoriesItems} from './categoriestype';
+export type {quizParamType} from './quiztypes';
+export type {userResultType} from './resultType';
